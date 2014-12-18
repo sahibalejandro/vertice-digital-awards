2f271dba3c251cf26fa6ed49e1a6646eb829e02b
Sahib J. Leo <sahib.alejandro@gmail.com>
2014-12-18 15:37:23 -0600

    Shuffle Nominees.
    Changed: Nominees are shuffled because are ordered by votes desc.

--- app/App/Repositories/NomineesRepository.php
+++ app/App/Repositories/NomineesRepository.php
@@ -38,6 +38,7 @@ class NomineesRepository extends Repository
             ->whereCategoryId($categoryId)
             ->groupBy('voted_user_id')
             ->with('user')
-            ->get($columns);
+            ->get($columns)
+            ->shuffle();
     }
 }
